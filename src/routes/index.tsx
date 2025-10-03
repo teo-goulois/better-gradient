1f9afed443a47bd91fae3201494f8ff41992910a
Téo Goulois <teo.goulois.dev@gmail.com>
2025-10-03 19:51:09 +0200

    feat: restructure App component layout and add footer for improved user experience
    - Replaced the outer div with a main element for better semantic structure and accessibility.
    - Introduced a footer with social media links to enhance user engagement and provide additional information.
    - Adjusted the main content area to ensure proper height calculation, improving layout consistency across different screen sizes.
    - Ensured consistent use of inferred types and added comments for clarity throughout the updated component.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -65,8 +65,8 @@ function App() {
         // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
         dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
       />
-      <div className="flex-1 w-full min-h-screen bg-bg relative overflow-hidden">
-        <div className="container mx-auto px-6 py-28 z-10 relative h-full flex items-center justify-center min-h-screen">
+      <main className="flex-1 w-full bg-bg relative overflow-hidden">
+        <div className="container mx-auto px-6 py-28 z-10 relative h-full flex items-center justify-center min-h-[calc(100dvh-4.25rem)]">
           <div className="max-w-3xl mx-auto text-center">
             <h1 className="mt-6 font-nohemi text-5xl md:text-6xl font-semibold tracking-tight text-neutral-900">
               Create subtle, modern mesh gradients
@@ -107,7 +107,64 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
+      </main>
+      <footer className="text-center  text-sm text-muted-fg/50  fixed bottom-0 left-0 right-0 pb-4">
+        <div className="flex flex-col justify-between container mx-auto gap-8 sm:flex-row sm:items-center">
+          <h2 className="text-sm font-medium">2025 Better Gradient</h2>
+          <nav
+            aria-labelledby="social-icons-heading"
+            className="flex items-center gap-4 "
+          >
+            <p className="text-base sr-only" id="social-icons-heading">
+              Follow us on social media
+            </p>
+            <a
+              className=" text-sm font-medium  hover:text-blue-600 dark:hover:text-blue-300"
+              href="https://x.com/teo_goulois"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="icon icon-tabler-layout-brand-x size-4"
+              >
+                <title>X</title>
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+                <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+              </svg>
+            </a>
+
+            {/* <a
+              className="text-base font-medium hover:text-blue-500  dark:hover:text-blue-400"
+              href="https://github.com/teo-goulois/better-gradient"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="icon icon-tabler-layout-brand-github size-4"
+              >
+                <title>Github</title>
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+              </svg>
+            </a> */}
+          </nav>
+        </div>
+      </footer>
     </>
   );
 }
